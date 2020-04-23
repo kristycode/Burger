9d418dc856f9812d041384fcdc9074c56d5cb90e
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-04-23 00:04:35 -0500

    is this working now

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Burger
-this is a burger repo for a burger app to eat burgers, store burgers, and burger burgers
+this is a burger repo for a burger app to eat burgers, store burgers, and unburger burgers
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('burger king big kids meal');
+INSERT INTO burgers (burger_name) VALUES ('the pete buttiburger with no mayo');
+INSERT INTO burgers (burger_name, devoured) VALUES ('kungfu kapowger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('theyve never seen a burger like this before', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('regular hamburger', true);
+
